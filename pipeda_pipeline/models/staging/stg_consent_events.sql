52ed5cd9897c97b5404942ce098432e308eabbf2
pprateekk <pprateek1204@gmail.com>
2026-06-01 11:28:33 -0400

    fix errors

--- pipeda_pipeline/models/staging/stg_consent_events.sql
+++ pipeda_pipeline/models/staging/stg_consent_events.sql
@@ -1,16 +1,16 @@
 with source as (
     select * from {{ source('raw', 'consent_events')}}
-)
+),
 
 cleaned as (
     select 
         event_id, 
         user_id, 
         lower(trim(event_type)) as event_type,
-        lower(trim(purpise)) as purpose,
+        lower(trim(purpose)) as purpose,
         lower(trim(channel)) as channel,
         policy_version_id, 
-        event_timestamp as event_timestamp_utc
+        event_timestamp as event_timestamp_utc,
         metadata,
         inserted_at
 
